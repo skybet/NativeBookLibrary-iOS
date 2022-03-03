fcc9c27ec1f2bc50aa43a2de3f913dfec5d184d4
Matthew Gallagher <matthew.gallagher@skybettingandgaming.com>
2022-03-03 18:38:11 +0000

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Matthew Gallagher on 03/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/NativeBookLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeBookLibraryApp.swift
+//  Shared
+//
+//  Created by Matthew Gallagher on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NativeBookLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
